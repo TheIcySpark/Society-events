d06a338a8296e3ded6a93abb488c820a9608dcb4
Edward997 <edward.navarrete7@gmail.com>
2024-04-24 15:30:54 -0600

    responisve ajustment

--- society-events-frontend/src/pages/HomePage.tsx
+++ society-events-frontend/src/pages/HomePage.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Box, Flex, Heading, Text, Button, Image, Stack, useColorModeValue } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import HeroImage from '../assets/images/hero.jpg'; // Importa la imagen del héroe
+import CreateEventForm from '../components/CreateEventForm';
 
 const HomePage: React.FC = () => {
   return (
-    <Flex align="center" justify="center" direction="column" minH="100vh">
-      <Box maxW="600px" textAlign="center" px={4}>
-        <Heading as="h1" size="2xl" mb={6}>
-          ¡Bienvenido a mi Página de Inicio!
-        </Heading>
-        <Text fontSize="xl" mb={8}>
-          Aquí puedes colocar una descripción breve de tu página de inicio y lo que ofrece tu sitio web o aplicación.
-        </Text>
-        <Button colorScheme="teal" size="lg" mb={4}>
-          Iniciar sesión
-        </Button>
-        <Button variant="outline" size="lg">
-          Registrarse
-        </Button>
+    <Flex>
+      {/* División izquierda (oculta en dispositivos móviles) */}
+      <Box bg="#050a11" display={{ base: 'none', md: 'block' }} w={{ base: '0%', md: '15%' }} />
+      
+      {/* División central */}
+      <Box w={{ base: '100%', md: '70%' }} overflowY="auto" h="80vh">
+        
       </Box>
       
+      {/* División derecha (oculta en dispositivos móviles) */}
+      <Box display={{ base: 'none', md: 'block' }} w={{ base: '0%', md: '15%' }} />
     </Flex>
   );
 };
