3de36575e4c420666ed397493e41c880f6e9d7ee
Luis Eduardo Zaldivar Miranda <lalozaldivarbarza@gmail.com>
2024-04-25 16:34:16 -0600

    ID and username send

--- society-events-backend/society_events_app/views.py
+++ society-events-backend/society_events_app/views.py
@@ -11,49 +11,54 @@ from society_events_app.serializers import (
     CommentSerializer,
     EventSerializer,
     EventViewSerializer,
+    UsernameTokenObtainPairSerializer,  # Corregido el nombre de la importación
 )
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .serializers import *
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, PermissionDenied
 from society_events_app.models import Event
 from society_events_app.serializers import EventSerializer
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+    serializer_class = UsernameTokenObtainPairSerializer
 
     def post(self, request, *args, **kwargs):
         print("Datos de la solicitud:", request.data)
-        return super().post(request, *args, **kwargs)
+        response = super().post(request, *args, **kwargs)
+        print("Datos de la respuesta:", response.data)
+        access_token = response.data.get('access')
+        print("Token de acceso:", access_token)
+        return response
 
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
 
-        if email and password:
-            user = User.objects.filter(email=email).first()
+def validate(self, attrs):
+    username = attrs.get('username')
+    password = attrs.get('password')
 
-            if user and user.check_password(password):
-                refresh = self.get_token(user)
-                data = {
-                    'refresh': str(refresh),
-                    'access': str(refresh.access_token),
-                    'user_id': user.id,
-                    'email': user.email,
-                }
-                return data
+    if username and password:
+        user = User.objects.filter(username=username).first()
 
-            raise AuthenticationFailed(
-                "No se encontró un usuario con las credenciales proporcionadas.")
-        else:
-            raise AuthenticationFailed(
-                "Se debe proporcionar tanto el correo electrónico como la contraseña.")
+        if user and user.check_password(password):
+            refresh = self.get_token(user)
+            data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user_id': user.id,
+                'username': user.username,
+            }
+            # Agregar este print para ver los datos del token
+            print("Datos del token:", data)
+            return data
+
+        raise AuthenticationFailed(
+            "No se encontró un usuario con las credenciales proporcionadas.")
+    else:
+        raise AuthenticationFailed(
+            "Se debe proporcionar tanto el nombre de usuario como la contraseña.")
 
 
 @api_view(['GET'])
@@ -93,6 +98,7 @@ def create_event(request):
         end_date = parse_datetime(mutable_data.get('end_date'))
         mutable_data['start_date'] = start_date.strftime('%Y-%m-%d %H:%M:%S')
         mutable_data['end_date'] = end_date.strftime('%Y-%m-%d %H:%M:%S')
+        # Aquí puedes establecer el ID del usuario creador
         mutable_data['creator'] = 1
         serializer = EventSerializer(data=mutable_data)
         if serializer.is_valid():
@@ -105,7 +111,8 @@ def create_event(request):
 @permission_classes([IsAuthenticated])
 def comment_list(request):
     if request.method == 'GET':
-        event_id = request.GET.get('event_id')  # Obtener el ID del evento de la URL
+        # Obtener el ID del evento de la URL
+        event_id = request.GET.get('event_id')
         comments = Comment.objects.filter(event_id=event_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
@@ -130,6 +137,7 @@ def event_detail(request, pk):
         raise NotFound("Event not found")
     except Exception as e:
         raise PermissionDenied(str(e))
+
 
 class CreateUserAPIView(APIView):
     def post(self, request):
